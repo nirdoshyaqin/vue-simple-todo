4c4954d1fc71a5c0d620fdccc03f1e8b1656c5dd
nirdosh <yuya-sato@nirvana-lab.com>
2020-01-10 12:57:23 +0900

    make it better

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav class="navbar is-white">
+    <div class="container">
+      <div class="navbar-menu is-active">
+        <div class="navbar-start">
+          <a
+            :class="{'is-active': filterOption === 'all'}"
+            class="navbar-item"
+            @click="emitFilter('all')"
+          >すべて</a>
+          <a
+            :class="{'is-active': filterOption === 'inprogress'}"
+            class="navbar-item"
+            @click="emitFilter('inprogress')"
+          >進行中</a>
+          <a
+            :class="{'is-active': filterOption === 'finished'}"
+            class="navbar-item"
+            @click="emitFilter('finished')"
+          >完了済み</a>
+          <a
+            :class="{'is-active': filterOption === 'notstarted'}"
+            class="navbar-item"
+            @click="emitFilter('notstarted')"
+          >未開始</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      filterOption: "all"
+    };
+  },
+  methods: {
+    emitFilter(filterOption) {
+      this.filterOption = filterOption;
+      this.$emit("filterSelected", filterOption);
+    }
+  }
+};
+</script>
+
+
+
+
+
+
+
